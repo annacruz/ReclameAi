5b3096ee1b49c10f23c9b16ef1d6231019750bc3
Anna Cruz <anna.cruz@gmail.com>
2019-08-27 12:22:13 -0300

    Adjust the vcr configuration This was necessary because of the missing configurations around which http connection library vcr has to use and also better configuration to work with tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,10 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,7 +61,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcs_cassettes'
+  config.cassette_library_dir = 'spec/fixtures/vcs_cassettes'
   config.hook_into :webmock
-  config.allow_http_connections_when_no_cassete = true
+  config.allow_http_connections_when_no_cassette = true
 end
